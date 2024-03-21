73af0c703934472990c0da19650f50316caac066
doratikeshwar <doratikeshwar111@gmail.com>
2024-03-21 21:47:17 +0530

    add second repository

---
+++ src/test/java/com/example/FirstTestProject/FirstTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
